70505ca57a43c4d3d7e7257af01810180c79ed87
ivchicano <46921761+ivchicano@users.noreply.github.com>
2020-06-06 21:01:29 +0200

    JWT Authentication Point now describes the error in case there is an unauthorized status error

--- vscode4teaching-server/src/main/java/com/vscode4teaching/vscode4teachingserver/security/jwt/JWTAuthenticationEntryPoint.java
+++ vscode4teaching-server/src/main/java/com/vscode4teaching/vscode4teachingserver/security/jwt/JWTAuthenticationEntryPoint.java
@@ -18,7 +18,7 @@ public class JWTAuthenticationEntryPoint implements AuthenticationEntryPoint, Se
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
             AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
     }
 
 }
