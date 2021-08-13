aae5cd1443b4f9989cda088a1692c874f3de6fb5
ivchicano <46921761+ivchicano@users.noreply.github.com>
2021-08-13 12:27:49 +0200

    Move controller to controller package

--- vscode4teaching-server/src/main/java/com/vscode4teaching/vscode4teachingserver/controllers/ViewController.java
+++ vscode4teaching-server/src/main/java/com/vscode4teaching/vscode4teachingserver/controllers/ViewController.java
@@ -1,4 +1,4 @@
-package com.vscode4teaching.vscode4teachingserver;
+package com.vscode4teaching.vscode4teachingserver.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
