108b12a363046876c464dbee58b31dbce295762a
Diego Guerrero <d.guerrero.2018@alumnos.urjc.es>
2022-03-20 17:05:01 +0100

    Bug fix: disposing Dashboard button when logout
    Dashboard status bar's button has been fixed and now disappears when logging out of V4T.

--- vscode4teaching-extension/src/extension.ts
+++ vscode4teaching-extension/src/extension.ts
@@ -91,7 +91,7 @@ export function activate(context: vscode.ExtensionContext) {
         coursesProvider.logout();
         if (showLiveshareBoardItem) {
             showLiveshareBoardItem.dispose();
-            showDashboardItem = undefined;
+            showLiveshareBoardItem = undefined;
         }
         if (showDashboardItem) {
             showDashboardItem.dispose();
